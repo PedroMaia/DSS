46b7756e18be1bcaa3d7a3beba29e249254fb548
lccaseiro58 <lccaseiro58@gmail.com>
2012-12-26 20:11:23 +0000

    Gets e Sets de Troca e Suspeita Dá erro pois o metodo clone() ainda não está definido para isso é necessário fazer os construtores

--- 2-Fase/DSS/src/Business/Suspeita.java
+++ 2-Fase/DSS/src/Business/Suspeita.java
@@ -4,4 +4,46 @@ public class Suspeita {
 	private Utilizador _queixoso;
 	private Produto _p;
 	private String _just;
+
+public Suspeita(Utilizador u,Produto p,String j){
+    _queixoso=u.clone();
+    _p=p.clone();
+    _just=j;
+}
+
+public Suspeita(Suspeita s){
+    _queixoso=s.getQueix();
+    _p=s.getProd();
+    _just=s.getJust();
+}
+
+public Utilizador getQueix(){
+    return _queixoso.clone();
+}
+
+public Produto getProd(){
+    return _p.clone();
+}
+
+public String getJust(){
+    return _just;
+}
+
+public void setQueixoso(Utilizador u){
+    _queixoso=u.clone();
+}
+
+public void setProd(Produto p){
+    _p=p.clone();
+}
+
+public void setJust(String j){
+    _just=j;
+}
+
+
+
+
+
+
 }
--- 2-Fase/DSS/src/Business/Troca.java
+++ 2-Fase/DSS/src/Business/Troca.java
@@ -7,5 +7,70 @@ public class Troca {
 	private Produto oferta;
 	boolean aceite;
         boolean concluido;
-	
+
+public Troca(Utilizador u1,Utilizador u2,Produto des,Produto of,boolean a,boolean conc){
+    _prop=u1.clone();
+    _convidado=u2.clone();
+    desejado=des.clone();
+    oferta=of.clone();
+    aceite=a;
+    concluido=conc;
+}
+
+public Troca(Troca t){
+    _prop=t.getProp();
+    _convidado=t.getConv();
+    desejado=t.getDesej();
+    oferta=t.getOfert();
+    aceite=t.getResult();
+    concluido=t.getConc();
+}
+
+public Utilizador getProp(){
+    return _prop.clone();
+}
+
+public Utilizador getConv(){
+    return _convidado.clone();
+}
+        
+public Produto getDesej(){
+    return desejado.clone();
+}        
+   
+public Produto getOfert(){
+    return oferta.clone();
+} 
+
+public Boolean getResult(){
+    return aceite;
+} 
+
+public Boolean getConc(){
+    return concluido;
+} 
+
+public void setProp(Utilizador u){
+    _prop=u.clone();
+}
+
+public void setConv(Utilizador u){
+    _convidado=u.clone();
+}
+
+public void setDesej(Produto p){
+    desejado=p.clone();
+}
+
+public void setOfert(Produto p){
+    oferta=p.clone();
+}
+
+public void setResult(Boolean b){
+    aceite = b;
+}
+
+public void setConc(Boolean b){
+    concluido = b;
+}
 }
