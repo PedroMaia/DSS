73eece3899e1f7950fe6d8487e50a00cbf0c48f3
pedro96 <pmiguelf15@gmail.com>
2013-01-02 20:18:30 +0000

    Gravei sem querer! I'm sorry people

--- 2-Fase/DSS/src/Business/Troca.java
+++ 2-Fase/DSS/src/Business/Troca.java
@@ -8,21 +8,21 @@ public class Troca {
 	private Produto desejado;
 	private Produto oferta;
 	private GregorianCalendar dataProposta;
-        private GregorianCalendar dataConfirmacao;
-        private GregorianCalendar dataConclusao;
-        private GregorianCalendar dataLimite;
-        private int id;
+	private GregorianCalendar dataConfirmacao;
+	private GregorianCalendar dataConclusao;
+	private GregorianCalendar dataLimite;
+	private int id;
 
     public Troca(Utilizador _prop, Utilizador _convidado, Produto desejado, Produto oferta, GregorianCalendar dataProposta, GregorianCalendar dataConfirmacao, GregorianCalendar dataConclusao, GregorianCalendar dataLimite, int id) {
-        this._prop = _prop;
-        this._convidado = _convidado;
-        this.desejado = desejado;
-        this.oferta = oferta;
-        this.dataProposta = dataProposta;
-        this.dataConfirmacao = dataConfirmacao;
-        this.dataConclusao = dataConclusao;
-        this.dataLimite = dataLimite;
-        this.id = id;
+		this._prop = _prop;
+		this._convidado = _convidado;
+		this.desejado = desejado;
+		this.oferta = oferta;
+		this.dataProposta = dataProposta;
+		this.dataConfirmacao = dataConfirmacao;
+		this.dataConclusao = dataConclusao;
+		this.dataLimite = dataLimite;
+		this.id = id;
     }
 
     public Utilizador getProp() {
