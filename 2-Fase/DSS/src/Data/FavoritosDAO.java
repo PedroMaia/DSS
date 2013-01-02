af2a694ec4bff8a1493152d719214e8763ed04b8
lccaseiro58 <lccaseiro58@gmail.com>
2013-01-02 18:40:13 +0000

    algumas Categorias

--- 2-Fase/DSS/src/Data/FavoritosDAO.java
+++ 2-Fase/DSS/src/Data/FavoritosDAO.java
@@ -23,52 +23,49 @@ public class FavoritosDAO {
     public FavoritosDAO(String username) {
         this.username = username;
     }
-    
-    public boolean add(Produto p) throws SQLException
-    {
+
+    public boolean add(Produto p) throws SQLException {
         Connection c = DataConnection.getDataConnection();
         PreparedStatement s = c.prepareStatement("insert into favorito values(?,?)");
-        s.setString(1,username);
+        s.setString(1, username);
         s.setInt(2, p.getId());
-        int res=s.executeUpdate();
+        int res = s.executeUpdate();
         c.close();
-        return (res<1);
+        return (res < 1);
     }
-    
-    public boolean contains(Produto p) throws SQLException
-    {
+
+    public boolean contains(Produto p) throws SQLException {
         Connection c = DataConnection.getDataConnection();
         PreparedStatement s = c.prepareStatement("select count(*) from favorito where usr=? and idp=?");
-        s.setString(1,username);
+        s.setString(1, username);
         s.setInt(2, p.getId());
         ResultSet rs = s.executeQuery();
-        int res=0;
-        if(rs.next()) res=rs.getInt(1);
+        int res = 0;
+        if (rs.next()) {
+            res = rs.getInt(1);
+        }
         c.close();
-        return (res<1);
+        return (res < 1);
     }
-    
-    public boolean delete(Produto p) throws SQLException
-    {
+
+    public boolean delete(Produto p) throws SQLException {
         Connection c = DataConnection.getDataConnection();
         PreparedStatement s = c.prepareStatement("delete from favorito where usr=? and idp=?");
-        s.setString(1,username);
-        s.setInt(2,p.getId());
-        int res=0;
-        res=s.executeUpdate();
-        return (res<0);
+        s.setString(1, username);
+        s.setInt(2, p.getId());
+        int res = 0;
+        res = s.executeUpdate();
+        return (res < 0);
     }
-    
-    public List<Produto> list() throws SQLException
-    {
+
+    public List<Produto> list() throws SQLException {
         Connection c = DataConnection.getDataConnection();
         PreparedStatement s = c.prepareStatement("select idp from favorito where usr=?");
         s.setString(1, username);
         ResultSet rs = s.executeQuery();
         ProdutosDAO p = new ProdutosDAO();
         List<Produto> res = new ArrayList<Produto>();
-        while(rs.next())
-        {
+        while (rs.next()) {
             res.add(p.get(rs.getInt(1)));
         }
         return res;
