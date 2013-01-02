c2af760231b01f24706c22b0d9b6211d0e751b68
pspkzar <cesar.perdigao92@gmail.com>
2013-01-02 14:22:50 +0000

    bug corrigido

--- 2-Fase/DSS/src/Data/ClassificacaoDAO.java
+++ 2-Fase/DSS/src/Data/ClassificacaoDAO.java
@@ -79,7 +79,7 @@ public class ClassificacaoDAO {
     public boolean add(Classificacao cl) throws SQLException
     {
         Connection c = DataConnection.getDataConnection();
-        PreparedStatement s = c.prepareStatement("select * from classificacoes where classificado = ? and classificador = ?");
+        PreparedStatement s = c.prepareStatement("select * from classificacao where classificado = ? and classificador = ?");
         s.setString(1, classificado);
         s.setString(2, cl.getClassificador().getUsername());
         int res=0;
