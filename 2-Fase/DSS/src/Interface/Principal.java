dd9acafe3830f6f71031bfa1056c46a8f83bde7a
PedroMaia <maiarib@gmail.com>
2013-01-04 11:27:33 +0000

    Revert "Interface	 Pesquisa:Algumas alterações"
    This reverts commit 10e3d408a1f93e2d5d1e310a1273413b6a5860eb.

--- 2-Fase/DSS/src/Interface/Principal.java
+++ 2-Fase/DSS/src/Interface/Principal.java
@@ -34,6 +34,7 @@ public class Principal extends javax.swing.JFrame {
     private void initComponents() {
 
         jDesktopPanel = new javax.swing.JDesktopPane();
+        jToolBar1 = new javax.swing.JToolBar();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItemNovaJanela = new javax.swing.JMenuItem();
@@ -42,6 +43,8 @@ public class Principal extends javax.swing.JFrame {
         jMenu2 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jToolBar1.setRollover(true);
 
         jMenu1.setText("File");
         jMenu1.addActionListener(new java.awt.event.ActionListener() {
@@ -85,16 +88,20 @@ public class Principal extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jDesktopPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 646, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jToolBar1, javax.swing.GroupLayout.DEFAULT_SIZE, 656, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(10, 10, 10)
+                        .addComponent(jDesktopPanel)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jDesktopPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 384, Short.MAX_VALUE)
+                .addComponent(jToolBar1, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jDesktopPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 370, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -105,7 +112,7 @@ public class Principal extends javax.swing.JFrame {
     {
        
        if(this.janelaBody==null) 
-           this.janelaBody=new PrincipalBody();
+           this.janelaBody=new PrincipalBody(this.sys);
        
        this.jDesktopPanel.add(this.janelaBody);
        this.janelaBody.setVisible(true);
@@ -230,6 +237,7 @@ public class Principal extends javax.swing.JFrame {
          */
         java.awt.EventQueue.invokeLater(new Runnable() {
 
+            @Override
             public void run() {
                 new Principal().setVisible(true);
             }
@@ -243,6 +251,7 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItemClient;
     private javax.swing.JMenuItem jMenuItemLogIn;
     private javax.swing.JMenuItem jMenuItemNovaJanela;
+    private javax.swing.JToolBar jToolBar1;
     // End of variables declaration//GEN-END:variables
     private PrincipalBody janelaBody;
     private LogIn loginJanela;
